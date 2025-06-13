4b5d0397674c7f97d95b4dfee07534b2649ec05f
nolanorhanOFF <166512738+nolanorhanOFF@users.noreply.github.com>
2025-06-13 10:58:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let player;
 const playlist = [
-  { id: "VT2qF97pQNw", title: "Musique 1" },
-  { id: "gTcGXHuLMwg", title: "Musique 2" },
-  { id: "Uhmg30gpTiQ", title: "Musique 3" },
-  { id: "Xjws8nS606Q", title: "Musique 4" },
-  { id: "3ArJlad2q74", title: "Musique 5" },
-  { id: "kPa7bsKwL-c", title: "Musique 6" },
-  { id: "fL0zhg3nBus", title: "Musique 7" },
+  { id: "VT2qF97pQNw", title: "GIMS - NINAO" },
+  { id: "gTcGXHuLMwg", title: "HEM - Zanotti (Clip Officiel)" },
+  { id: "Uhmg30gpTiQ", title: "Anyme - Shamballa feat. Shaydee's (Clip officiel)" },
+  { id: "Xjws8nS606Q", title: "Dr. Yaro - Minimum ça (Clip Officiel)" },
+  { id: "3ArJlad2q74", title: "Vianney, ‪MikaSoundsOfficial‬ - Keep it simple (feat. Mika) (clip officiel)" },
+  { id: "kPa7bsKwL-c", title: "Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video)" },
+  { id: "fL0zhg3nBus", title: "nolannax - La nuit éternel (clip officiel)" },
   { id: "RgKAFK5djSk", title: "Wiz Khalifa - See You Again" },
   { id: "2Vv-BfVoq4g", title: "Ed Sheeran - Perfect" },
   { id: "09R8_2nJtjg", title: "Maroon 5 - Sugar" }
